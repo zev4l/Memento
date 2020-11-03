42f801c92950cb48b64f604c6c86b645cb174853
ObiMike16 <61832065+ObiMike16@users.noreply.github.com>
2020-11-03 13:02:43 +0000

    1

---
+++ Scripts/comportamento.js
@@ -0,0 +1,12 @@
+/* Interação com Computadores 2020/2021
+Grupo 23:
+Pedro Luís 
+Miguel Lages 54975
+José Almeida
+*/
+
+/*eslint-env es6*/
+/*eslint-env browser*/
+// Impede alguns erros fáceis de cometer.
+"use strict";
+
