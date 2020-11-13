0bef89282c5b4c27742dd7ce39794e3d73238fd6
ObiMike16 <61832065+ObiMike16@users.noreply.github.com>
2020-11-13 18:11:46 +0000

    settings

---
+++ scripts/settings.js
@@ -0,0 +1,6 @@
+/* Grupo: 23, Número: 55373, Nome: José Almeida, PL: 12 /
+/ Grupo: 23, Número: 54975, Nome: Miguel Lages, PL: 12 /
+/ Grupo: 23, Número: 53469, Nome: Pedro Luís, PL: 12 */
+
+/* DEFINIÇÃO DE CONSTANTES E VARIÁVEIS GLOBAIS */
+
